bdfa0b3b15963fb106578ef5b327ed196f67a453
Reinhard Feger <47209718+rf-eng@users.noreply.github.com>
2021-10-27 16:11:34 +0200

    CI: Initial commit of CI build files

---
+++ ci_tools/ci.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if which nproc > /dev/null; then
+    MAKEOPTS="-j$(nproc)"
+else
+    MAKEOPTS="-j$(sysctl -n hw.ncpu)"
+fi
+
+########################################################################################
+# general helper functions
+
+function ci_pkg_install {
+    # Compiling OpenEMS may require installing the following packages:
+    apt-get install cmake qt4-qmake libtinyxml-dev libcgal-dev libvtk5-qt4-dev
+    # Compiling hyp2mat may require installing the following packages:
+    apt-get install gengetopt help2man groff pod2pdf bison flex libhpdf-dev libtool
+}
+
+
+########################################################################################
+
+function ci_setup {
+    ci_pkg_install
+}
+
+function ci_build {
+    git submodule update --init AppCSXCAD
+    git submodule update --init CSXCAD
+    git submodule update --init CTB
+    git submodule update --init QCSXCAD
+    git submodule update --init fparser
+    git submodule update --init hyp2mat
+    git submodule update --init openEMS
+    update_openEMS.sh /build_output --disable-GUI
+}
