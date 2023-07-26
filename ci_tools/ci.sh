9aa72dfee9cf439e82577c33812122ecf8b4b6cc
Reinhard Feger <47209718+rf-eng@users.noreply.github.com>
2023-07-26 19:08:56 +0200

    CI: Fix paths for setup.py calls

--- ci_tools/ci.sh
+++ ci_tools/ci.sh
@@ -32,13 +32,15 @@ function ci_build {
     git submodule update --init hyp2mat
     git submodule update --init openEMS
     cd $GITHUB_WORKSPACE
-    ./update_openEMS.sh $GITHUB_WORKSPACE/build_output --disable-GUI --python
+    ./update_openEMS.sh $GITHUB_WORKSPACE/build_output --python
+
     cd $GITHUB_WORKSPACE/CSXCAD/python
-    python setup.py build_ext -I$GITHUB_WORKSPACE/build_output/opt/openEMS/include -L$GITHUB_WORKSPACE/build_output/opt/openEMS/lib -R$GITHUB_WORKSPACE/build_output/opt/openEMS/lib
+    python setup.py build_ext -I$GITHUB_WORKSPACE/build_output/openEMS/include -L$GITHUB_WORKSPACE/build_output/openEMS/lib -R$GITHUB_WORKSPACE/build_output/openEMS/lib
     python setup.py bdist_wheel
+    pip3 install $GITHUB_WORKSPACE/openEMS-Project/CSXCAD/python/dist/CSXCAD-0.6.2-cp310-cp310-linux_x86_64.whl
 
     cd $GITHUB_WORKSPACE/openEMS/python
-    python setup.py build_ext -I$GITHUB_WORKSPACE/build_output/opt/openEMS/include -L$GITHUB_WORKSPACE/build_output/opt/openEMS/lib -R$GITHUB_WORKSPACE/build_output/opt/openEMS/lib
+    python setup.py build_ext -I$GITHUB_WORKSPACE/build_output/openEMS/include -L$GITHUB_WORKSPACE/build_output/openEMS/lib -R$GITHUB_WORKSPACE/build_output/openEMS/lib
     python setup.py bdist_wheel
     pip3 install $GITHUB_WORKSPACE/openEMS-Project/openEMS/python/dist/openEMS-0.0.33-cp310-cp310-linux_x86_64.whl
     #pip3 wheel $GITHUB_WORKSPACE/CSXCAD/python --global-option=build_ext --global-option=-L$GITHUB_WORKSPACE/build_output/lib --global-option=-I$GITHUB_WORKSPACE/build_output/include --global-option=-R$GITHUB_WORKSPACE/build_output/lib
